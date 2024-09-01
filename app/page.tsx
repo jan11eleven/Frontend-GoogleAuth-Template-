2d43c891628e9a1b32d0c62fe795db827d1d78bc
Jan Eleven Salvador <2.janelevensalvador@gmail.com>
2024-09-01 17:55:38 +0800

    git init

---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import BackendUrl from "./utils/BackendUrlBuilder";
+import AuthenticateUser from "./utils/AuthenticateUser";
+
+export default function Home() {
+	const [isAuthenticated, setIsAuthenticated] = useState<Boolean | null>(null);
+	const router = useRouter();
+
+	const signInToGoogleHandler = async () => {
+		window.location.href = `${BackendUrl}/auth/google`;
+	};
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			const result = await AuthenticateUser();
+
+			if (result) {
+				setIsAuthenticated(result.isAuthenticated);
+			} else {
+				setIsAuthenticated(false);
+			}
+		};
+
+		checkAuth();
+	}, []);
+
+	useEffect(() => {
+		if (isAuthenticated === true) {
+			router.push("/dashboard"); // Redirect to the dashboard if authenticated
+		}
+	}, [isAuthenticated, router]);
+
+	if (isAuthenticated === null) {
+		return <>Loading...</>;
+	}
+
+	return (
+		<main className="">
+			<div>
+				<h1>Sign in</h1>
+				<button className="button facebook" onClick={signInToGoogleHandler}>
+					Sign in with Google
+				</button>
+			</div>
+		</main>
+	);
+}
